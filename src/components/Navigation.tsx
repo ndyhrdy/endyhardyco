2e571c70070584ed7d65db45d4bb64f286adefac
Endy Hardy <endy0611@gmail.com>
2020-10-21 15:27:37 +0700

    Add Navbar

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,92 +1,103 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import Burger from "./Burger";
 import { useState } from "react";
+
+import config from "../lib/config";
 
 export default function Navigation() {
   const router = useRouter();
-  const [active, setActive] = useState(false);
-  return (
-    <>
-      <Burger active={active} onClick={() => setActive(!active)} />
-      <div className={"container " + (active ? "active" : "")}>
-        <ul>
-          <li>
-            <Link href="/">
-              <a className={router.pathname === "/" ? "active" : null}>about</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/posts">
-              <a
-                className={
-                  router.pathname.startsWith("/posts") ? "active" : null
-                }
-              >
-                blog
-              </a>
-            </Link>
-          </li>
-        </ul>
-        <style jsx>
-          {`
-            .container {
-              width: 0;
-            }
-            ul {
-              opacity: 0;
-              width: 100%;
-              height: 100vh;
-              text-align: right;
-              list-style: none;
-              margin: 0;
-              padding: 0;
-              position: fixed;
-              top: 0;
-              background-color: #fff;
-              display: flex;
-              flex-direction: column;
-              justify-content: center;
-              z-index: 1;
-              transform: translateY(100%);
-              transition: opacity 200ms;
-            }
-            .active ul {
-              opacity: 1;
-              transform: translateY(0);
-            }
-            li {
-              margin-bottom: 1.75rem;
-              font-size: 2rem;
-              padding: 0 1.5rem 0 0;
-            }
-            li:last-child {
-              margin-bottom: 0;
-            }
-            .active {
-              color: #222;
-            }
+  const links = [
+    { to: "/about", text: "About", active: router.pathname === "/about" },
+    {
+      to: "/portfolio",
+      text: "Portfolio",
+      active: router.pathname.startsWith("/portfolio"),
+    },
+    {
+      to: "/services",
+      text: "Services",
+      active: router.pathname === "/services",
+    },
+    {
+      to: "/posts",
+      text: "Blog",
+      active: router.pathname.startsWith("/posts"),
+    },
+    { to: "/contact", text: "Contact", active: router.pathname === "/contact" },
+  ];
 
-            @media (min-width: 769px) {
-              .container {
-                width: 7rem;
-                display: block;
-              }
-              ul {
-                opacity: 1;
-                width: 7rem;
-                top: auto;
-                display: block;
-                transform: translateY(0);
-              }
-              li {
-                font-size: 1rem;
-                padding: 0;
-              }
-            }
-          `}
-        </style>
+  const [expanded, setExpanded] = useState<boolean>(false);
+
+  return (
+    <div
+      className={`flex flex-col items-stretch md:block ${
+        expanded ? "h-screen" : ""
+      }`}
+    >
+      <div className="bg-white border-b">
+        <div className="border-t-8 border-teal-600"></div>
+        <div className="container h-32 flex items-center justify-between">
+          <Link href="/">
+            <a>
+              <img
+                src={config.logo_url}
+                alt={config.site_title}
+                className="h-20"
+              />
+            </a>
+          </Link>
+          <ul className="hidden md:flex items-center text-lg text-gray-600 md:-mr-4">
+            {links.map((link) => {
+              return (
+                <li key={link.to}>
+                  <Link href={link.to}>
+                    <a
+                      className={`block px-4 py-2 hover:text-teal-600 ${
+                        link.active ? "text-teal-500" : ""
+                      }`}
+                    >
+                      {link.text}
+                    </a>
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+          <button
+            className="md:hidden"
+            onClick={() => {
+              setExpanded(!expanded);
+            }}
+          >
+            <i className="block my-2 border-b-2 border-gray-600 w-8"></i>
+            <i className="block my-2 border-b-2 border-gray-600 w-8"></i>
+            <i className="block my-2 border-b-2 border-gray-600 w-8"></i>
+          </button>
+        </div>
       </div>
-    </>
+      {expanded && (
+        <div className="flex-1 bg-gray-200 flex flex-col justify-end">
+          <div className="container py-6">
+            <ul className="text-gray-500">
+              {links.map((link) => {
+                return (
+                  <li>
+                    <Link href={link.to}>
+                      <a
+                        className={`block text-xl py-1 ${
+                          link.active ? "text-teal-500" : ""
+                        }`}
+                      >
+                        {link.text}
+                      </a>
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </div>
+      )}
+    </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  purge: ["./components/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+    },
+    fontSize: {
+      xs: defaultTheme.fontSize.sm,
+      sm: defaultTheme.fontSize.base,
+      base: defaultTheme.fontSize.lg,
+      lg: defaultTheme.fontSize.xl,
+      xl: defaultTheme.fontSize["2xl"],
+      "2xl": defaultTheme.fontSize["3xl"],
+      "3xl": defaultTheme.fontSize["4xl"],
+      "4xl": defaultTheme.fontSize["5xl"],
+      "5xl": defaultTheme.fontSize["6xl"],
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+    },
+
+    extend: {
+      fontFamily: {
+        sans: ["Source Sans Pro", ...defaultTheme.fontFamily.sans],
+        heading: ["Oswald", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
